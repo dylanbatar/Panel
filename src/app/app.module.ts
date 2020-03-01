01b0cadd7d6a82b53c603669ac97232954e1bdbe
dylanbatar <dylanbatar2@gmail.com>
2020-03-01 15:45:48 -0500

    Separacion de Modulos , Simulacion de servicios para el menu , Cambio de temas del panel , cambios en los breadCrumbs y title de la aplicacion

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,7 +7,7 @@ import { AppComponent } from "./app.component";
 import { LoginComponent } from "./views/login/login.component";
 import { RegistroComponent } from "./views/registro/registro.component";
 import { AdminlayoutModule } from "./views/dashboard/adminlayout.module";
-
+import { RouterModule } from '@angular/router';
 
 
 @NgModule({
@@ -21,6 +21,7 @@ import { AdminlayoutModule } from "./views/dashboard/adminlayout.module";
     BrowserModule,
     AppRoutingModule,
     AdminlayoutModule,
+    RouterModule
   ],
   providers: [],
   bootstrap: [AppComponent]
